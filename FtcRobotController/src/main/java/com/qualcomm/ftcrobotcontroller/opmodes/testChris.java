370ef7c8ed1bd136bd13f818483d696a4ae391db
BuildTools <unconfigured@null.spigotmc.org>
2015-10-09 23:16:41 -0500

    test file vreated on 10-9-15

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/testChris.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/testChris.java
@@ -3,8 +3,10 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 /**
  * Created by owner on 10/9/2015.
  */
-public class testChris {
+public class testChris extends OpMode {
+
+    DcMotor
+
 }
 //test update
 
-//todo stuffs in this document
