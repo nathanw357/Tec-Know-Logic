cf4c9ada1710ca5139eb91580b5bfd0404ec3a00
nicol <nicole.f.heath@gmail.com>
2015-10-14 19:24:39 -0500

    Trigger Knocker Test

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TriggerTest.java
@@ -0,0 +1,13 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by nicol on 10/14/2015.
+ */
+
+public class TriggerTest extends OpMode {
+
+    DcMotor
+}
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/testChris.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/testChris.java
@@ -4,7 +4,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  * Created by owner on 10/9/2015.
  */
 public class testChris {
-Dc
+
 
 }
 
