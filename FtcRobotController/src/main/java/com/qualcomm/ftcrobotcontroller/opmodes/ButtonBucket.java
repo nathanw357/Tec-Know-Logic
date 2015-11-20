c967e9063e956692ea78ea695c3f7f15b8e6d5e4
NicoleH1306 <nicole.f.heath@gmail.com>
2015-11-19 19:17:45 -0600

    Added bucket program

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ButtonBucket.java
@@ -0,0 +1,82 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+/**
+ * Created by nicole on 10/15/2015.
+ */
+public class ButtonBucket extends OpMode {
+
+    //Servo at the base of the cowcatcher
+    //Control up and down motion
+    Servo bucket;
+
+    //The position of the servo.
+    // 0.2 is up, 0.5 is middle, and 0.9 is down.
+    double bucketPosition = 0.7;
+
+
+    //checks the states of the button and trigger
+    int buttonState = 0;
+    int triggerState = 0;
+
+
+    public void init(){
+
+        bucket = hardwareMap.servo.get("bucket");
+
+    }
+
+    public void loop() {
+
+        //right bumper activates upward movement
+        //The right trigger activates downward movement
+        boolean bucketUpButton = gamepad1.left_bumper;
+        float bucketDownButton = gamepad1.left_trigger;
+
+
+        if(bucketUpButton){
+            buttonState = 1;
+        }
+        else {
+            if(buttonState == 1) {
+
+                bucketPosition += 0.1;
+
+                buttonState = 0;
+            }
+        }
+
+
+        if(bucketDownButton >= 0.5){
+            triggerState = 1;
+        }
+        else{
+            if(triggerState == 1){
+
+                bucketPosition -= 0.1;
+
+                triggerState = 0;
+            }
+        }
+
+        if(triggerState == 1 && buttonState == 1){
+            triggerState = 0;
+            buttonState = 0;
+        }
+
+
+
+        bucket.setPosition(bucketPosition);
+
+        telemetry.addData("cowcatcher", "position:  " + String.format("%.2f", bucketPosition));
+
+
+
+
+
+    }
+
+}
