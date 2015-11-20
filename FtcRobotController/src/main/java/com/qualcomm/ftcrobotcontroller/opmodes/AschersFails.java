3ae74f8fcf1e9da595221891c789f3eee481f677
CStrnad <chrisstrnad@gmail.com>
2015-11-19 20:27:15 -0600

    Edited ChrisSquareTest. Created ChrisCircleTest and AschersFails. ChrisCircleTest completed. All need testing

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AschersFails.java
@@ -0,0 +1,22 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.*;
+
+/**
+ * Created by Ascher on 11/19/2015.
+ */
+public class AschersFails {
+    DcMotor leftMotor;
+
+
+public void () {
+    leftMotor = hardwareMap.dcMotor.get("leftMotorFront");
+    rightMotor = hardwareMap.dcMotor.get("rightMotorFront");
+    rightMotor.setDirection(Direction.REVERSE);
+    leftMotor2 = hardwareMap.dcMotor.get("leftMotorRear");
+    rightMotor2 = hardwareMap.dcMotor.get("rightMotorRear");
+    rightMotor2.setDirection(Direction.REVERSE);
+ }
+}
