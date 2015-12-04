f5fc5cc8206b78aac0aab338ff0c89d9c6d4b96c
Nicole Heath <nicole.f.heath@gmail.com>
2015-12-03 21:00:51 -0600

    Cleaned code up and began mapping Analog Sticks to Servos
    - Nathan

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Main.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Main.java
@@ -3,45 +3,66 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
-
-/**
- * Created by nicole on 10/15/2015.
- */
 public class Main extends OpMode {
+    final static double WRIST_MIN_RANGE = 0.20;
+    final static double WRIST_MAX_RANGE = 0.7;
 
-    //Servo at the base of the bucket
-    //Control up and down motion
+    //  Position of the arm servo
+    double wristPosition;
+
+    //  Amount to change the arm servo position by
+    double wristDelta = 0.1;
+
+//    Servo at the base of the bucket
+
     Servo bucketLeft;
     Servo bucketRight;
 
-    //Servo at the base of the cowcatcher
+//    Servo at the base of the cowcatcher
+
     Servo cowCatcher;
 
-    //Drive train motors
+//    Claw & Wrist Servos
+
+    Servo wrist;
+    Servo claw;
+
+//    Elbow DcMotor
+
+    DcMotor elbow;
+
+//    Shoulder X & Y
+    DcMotor ShoulderX;
+    DcMotor ShoulderY;
+
+//    Drive train motors
+
     DcMotor leftMotorFront;
     DcMotor leftMotorRear;
     DcMotor rightMotorFront;
     DcMotor rightMotorRear;
 
-    //The position of the servo.
+//    The position of the servo.
+
     double bucketLeftPosition = 0.3;
     double bucketRightPosition = 0.7;
 
-    //The position of the servo.
-    // 0.2 is down, 0.5 is middle, and 0.9 is up.
+//    The position of the servo.
+//     0.2 is down, 0.5 is middle, and 0.9 is up.
     double cowPosition = 0.2;
 
-    //A simplified position method.
-    //1 is down, 2 is middle, and 3 is up
+//    A simplified position method.
+//    1 is down, 2 is middle, and 3 is up
     int position = 1;
 
 
-    //checks the states of the right bumper and right trigger
+//    checks the states of the right bumper and right trigger
     int rightButtonState = 0;
     int rightTriggerState = 0;
 
-    //checks the states of the left bumper and left trigger
+//    checks the states of the left bumper and left trigger
     int leftButtonState = 0;
     int leftTriggerState = 0;
 
@@ -49,21 +70,28 @@ public class Main extends OpMode {
     public void init(){
 
 
-        //Maps the bucket servos
+//      Maps the bucket servos
         bucketLeft = hardwareMap.servo.get("bucketLeft");
         bucketRight = hardwareMap.servo.get("bucketRight");
 
-        //Maps the cowcathcer's motor
+//      Maps the cowcathcer's motor
         cowCatcher = hardwareMap.servo.get("cowCatcher");
 
-        //Maps the drive train motors
+//      Maps the drive train motors
         leftMotorFront = hardwareMap.dcMotor.get("leftMotorFront");
         leftMotorRear = hardwareMap.dcMotor.get("leftMotorRear");
         rightMotorFront = hardwareMap.dcMotor.get("rightMotorFront");
         rightMotorRear = hardwareMap.dcMotor.get("rightMotorRear");
 
+//      Maps the Elbow & Wrist motors
+        elbow = hardwareMap.dcMotor.get("elbow");
+        wrist = hardwareMap.servo.get("wrist");
 
-        //Setting the motors in reverse
+//      Maps the Shoulder X & Y
+        ShoulderX = hardwareMap.dcMotor.get("ShoulderX");
+        ShoulderY = hardwareMap.dcMotor.get("ShoulderY");
+
+//      Setting the motors in reverse
         leftMotorRear.setDirection(DcMotor.Direction.REVERSE);
         rightMotorFront.setDirection(DcMotor.Direction.REVERSE);
 
@@ -76,16 +104,66 @@ public class Main extends OpMode {
         boolean bucketUpButton = gamepad1.left_bumper;
         float bucketDownButton = gamepad1.left_trigger;
 
-        //right bumper activates upward movement
-        //The right trigger activates downward movement
+//      Right bumper activates upward movement
+//      The right trigger activates downward movement
         boolean cowUpButton = gamepad1.right_bumper;
         float cowDownButton = gamepad1.right_trigger;
 
-        //Drive train controls
+//      Drive train controls
         double leftY = -gamepad1.left_stick_y;
         double rightY = gamepad1.right_stick_y;
 
-        //Reads the right bumper and trigger to move the bucket
+//      Elbow & Wrist controls
+        double rightY2 = gamepad2.right_stick_y;
+        double rightX2 = gamepad2.right_stick_x;
+
+//      ShoulderX & ShoulderY controls
+        double leftY2  = -gamepad2.left_stick_y;
+        double leftX2 = -gamepad2.left_stick_x;
+
+//      Clip the position values of wrist so that they never exceed their allowed range.
+        wristPosition = Range.clip(wristPosition, WRIST_MIN_RANGE, WRIST_MAX_RANGE);
+
+//      Squares output of joysticks to create a smaller number; adds more control over DcMotor speed
+        rightY2 = Math.pow(rightY2, 2) * Math.signum(rightY2);
+        rightY = Math.pow(rightY, 2) * Math.signum(rightY);
+        leftY = Math.pow(leftY, 2) * Math.signum(leftY);
+        leftY2 = Math.pow(leftY2, 2) * Math.signum(leftY2);
+        leftX2 = Math.pow(leftX2, 2) * Math.signum(leftX2);
+        rightX2 = Math.pow(rightX2, 2) * Math.signum(rightX2);
+
+
+//      Write position values to the wrist & claw servo
+//      Claw open and close
+        if(gamepad2.right_bumper = true) {
+            claw.setPosition(1);
+        }
+
+        else {
+            claw.setPosition(0);
+        }
+
+//      Wrist Position
+        if(rightY2 > 0) {
+
+            wrist.setPosition(rightY2/2 + 0.5);
+        }
+
+        else {
+
+        }
+
+//      Set power to the elbow & shoulder's X & Y
+//      Elbow DC power
+        elbow.setPower(rightX2);
+
+//      ShoulderX
+        ShoulderX.setPower(leftX2);
+
+//      ShoulderY
+        ShoulderY.setPower(leftY2);
+
+//        Reads the right bumper and trigger to move the bucket
         if(bucketUpButton){
             rightButtonState = 1;   //Checks the state of the right button
         }
@@ -162,28 +240,36 @@ public class Main extends OpMode {
             leftButtonState = 0;
         }
 
-        //Changes the speed of the motors
-        leftY = Math.pow(leftY, 2) * Math.signum(leftY);
-        rightY = Math.pow(rightY, 2) * Math.signum(rightY);
-
-
-        //Setting postion of bucket
+//      Setting position of bucket
         bucketLeft.setPosition(bucketLeftPosition);
         bucketRight.setPosition(bucketRightPosition);
 
-        //Sets postion of cowcatcher
+//      Sets position of cowcatcher
         cowCatcher.setPosition(cowPosition);
 
-        //Sets speed on the drive train
+//      Sets speed on the drive train
         leftMotorFront.setPower(leftY);
         leftMotorRear.setPower(leftY);
         rightMotorFront.setPower(rightY);
         rightMotorRear.setPower(rightY);
 
+
+//      Telemetry data sent back to driver station
+        telemetry.addData("Text", "** Robot Data**");
         telemetry.addData("leftbucket", "position:  " + String.format("%.2f", bucketLeftPosition));
         telemetry.addData("rightbucket", "position:" + String.format("%.2f", bucketRightPosition));
-
-
+        telemetry.addData("leftMotorFront", "position:  " + String.format("%.2f", leftMotorFront));
+        telemetry.addData("leftMotorRear", "position:" + String.format("%.2f", leftMotorRear));
+        telemetry.addData("rightMotorFront", "position:  " + String.format("%.2f", rightMotorFront));
+        telemetry.addData("rightMotorRear", "position:" + String.format("%.2f", rightMotorRear));
+        telemetry.addData("elbow", "elbow:  " + String.format("%.2f", elbow));
+        telemetry.addData("wrist", "wrist:  " + String.format("%.2f", wristPosition));
+        telemetry.addData("leftY tgt pwr", "leftY  pwr: " + String.format("%.2f", leftY));
+        telemetry.addData("rightY tgt pwr", "rightY  pwr: " + String.format("%.2f", rightY));
+        telemetry.addData("rightX2 tgt pwr", "rightX2 pwr: " + String.format("%.2f", rightX2));
+        telemetry.addData("rightY2 tgt pwr", "rightY2 pwr: " + String.format("%.2f", rightY2));
+        telemetry.addData("leftY2 tgt pwr", "leftY2 pwr: " + String.format("%.2f", leftY2));
+        telemetry.addData("leftX2 tgt pwr", "leftX2 pwr: " + String.format("%.2f", leftX2));
 
 
 
