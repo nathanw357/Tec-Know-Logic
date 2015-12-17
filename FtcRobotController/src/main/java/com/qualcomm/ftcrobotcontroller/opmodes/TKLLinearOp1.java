314371f7a24a7b71eb6b1aa187b2edd83b1fe6c2
CStrnad <chrisstrnad@gmail.com>
2015-12-16 18:35:15 -0600

    Linear Op Mode number 1

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TKLLinearOp1.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TKLLinearOp1.java
@@ -27,18 +27,64 @@ public class TKLLinearOp1 extends LinearOpMode {
 
         waitForStart();
 
-        //Drive forward
+        //--------Go Forward--------------
+        telemetry.addData("text", "Moving Forward!");
         leftMotor.setPower(.75);
         rightMotor.setPower(.75);
         leftMotor2.setPower(.75);
         rightMotor2.setPower(.75);
 
+        //wait for 5 seconds
         wait(5000);
+        telemetry.clearData();
 
+        //Halt Motors
         leftMotor.setPower(0);
         rightMotor.setPower(0);
         leftMotor2.setPower(0);
         rightMotor2.setPower(0);
+
+        //--------- Right Turn -------------
+        wait(500);
+
+        //turn right
+        telemetry.addData("text", "Turning Right!");
+        leftMotor.setPower(.75);
+        rightMotor.setPower(-0.75);
+        leftMotor2.setPower(.75);
+        rightMotor2.setPower(-0.75);
+
+        //wait for 2.5 seconds
+        wait(2500);
+        telemetry.clearData();
+
+        //Halt Motors
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+        leftMotor2.setPower(0);
+        rightMotor2.setPower(0);
+
+//      ---------- Forward -------------
+//        Move forward onto and up mountain
+        wait(500);
+        telemetry.addData("text", "Onward!");
+        leftMotor.setPower(.75);
+        rightMotor.setPower(.75);
+        leftMotor2.setPower(.75);
+        rightMotor2.setPower(.75);
+
+        //wait 6 seconds
+        wait(6000);
+        telemetry.clearData();
+
+        //Halt Motors
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+        leftMotor2.setPower(0);
+        rightMotor2.setPower(0);
+
+
+
 
 
         }
