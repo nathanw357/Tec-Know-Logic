6bccc896b1ba709da1d1aeb9e4fa0c0100853e78
Nicole Heath <nicole.f.heath@gmail.com>
2016-01-10 16:07:28 -0600

    Reworked encoder code to be LinearOpMode instead of OpMode

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Encoder.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Encoder.java
@@ -1,67 +1,75 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 
-/**
- * Created by chris on 12/6/2015.
- */
-public class Encoder extends OpMode {
+
+public class Encoder extends LinearOpMode {
 
     DcMotor leftMotor;
     DcMotor rightMotor;
     DcMotor leftMotor2;
     DcMotor rightMotor2;
 
-    final static int ENCODER_CFR = 1440;
-    final static double GEAR_RATIO = 1;
-    final static int WHEEL_DIAMETER = 4;
-    final static int DISTANCE = 36;
-
-    final static double CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
-    final static double ROTATIONS = DISTANCE / CIRCUMFERENCE;
-    final static double COUNTS = ENCODER_CFR * ROTATIONS *GEAR_RATIO;
-
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
+
         leftMotor = hardwareMap.dcMotor.get("leftMotorFront");
         leftMotor2 = hardwareMap.dcMotor.get("leftMotorRear");
         rightMotor = hardwareMap.dcMotor.get("rightMotorFront");
         rightMotor2 = hardwareMap.dcMotor.get("rightMotorRear");
 
-        //rightMotor.setDirection(DcMotor.Direction.REVERSE);
         rightMotor2.setDirection(DcMotor.Direction.REVERSE);
-        leftMotor2.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
 
         leftMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        leftMotor2.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         rightMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        rightMotor2.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-    }
 
-    @Override
-    public void start() {
-        leftMotor.setTargetPosition((int) COUNTS);
-        rightMotor.setTargetPosition((int) COUNTS);
-        leftMotor2.setTargetPosition((int) COUNTS);
-        rightMotor2.setTargetPosition((int) COUNTS);
+        leftMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        rightMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
 
-        leftMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        leftMotor2.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        rightMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        rightMotor2.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        telemetry.addData("text", "Run To Position");
+
+        leftMotor.setTargetPosition((int) COUNTS(36));
+        rightMotor.setTargetPosition((int) COUNTS(36));
+
+        telemetry.addData("text", "Set Target Position");
 
         leftMotor.setPower(0.5);
         rightMotor.setPower(0.5);
         leftMotor2.setPower(0.5);
         rightMotor2.setPower(0.5);
+        telemetry.addData("text", "Set Power Forward");
+
+        while(rightMotor.getCurrentPosition() < (int) COUNTS(36) - 10) {
+
+            telemetry.addData("count", rightMotor.getCurrentPosition());
+            telemetry.addData("Motor Target", COUNTS(36));
+        }
+
+        rightMotor.setPowerFloat();
+        rightMotor2.setPowerFloat();
+        leftMotor.setPowerFloat();
+        leftMotor2.setPowerFloat();
+
+        telemetry.addData("text", "Set Power Float");
+
+        telemetry.addData("text", "End");
     }
 
-    @Override
-    public void loop() {
-        telemetry.addData("Motor Target", COUNTS);
-        telemetry.addData("LeftPosition", leftMotor.getCurrentPosition());
-        telemetry.addData("Right Position", rightMotor.getCurrentPosition());
+    private double COUNTS(double DISTANCE) {
+        final int ENCODER_CFR = 1440;            //Encoder Counts per Revolution
+        final int GEAR_RATIO = 1;             //Gear Ratio
+        final double WHEEL_DIAMETER = 3.1715;    //Wheel diameter
+
+        double CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
+        double ROTATIONS = DISTANCE / CIRCUMFERENCE;
+        double COUNT = ENCODER_CFR * ROTATIONS * GEAR_RATIO;
+
+        return COUNT;
+
     }
 }
